f32f788d92587105c7e557a87521ef0463c31763
kv-22 <kavyadhawan22@yahoo.com>
2025-12-13 21:22:06 +0300

    change origin

--- backend/api.py
+++ backend/api.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from main import translate, q_and_a
 import logging
+import os
 
 # Configure logging
 logging.basicConfig(
@@ -38,13 +39,12 @@ class ChatRequest(BaseModel):
 
 app = FastAPI()
 
+origins = os.getenv("CORS_ORIGINS", "")
+
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173", 
-        "",  # Your computer's IP on hotspot
-    ],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
